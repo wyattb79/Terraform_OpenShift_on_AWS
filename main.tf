7fd80a2a1f6739571a7e40a2ae5e83551bf27c80
wyattb79 <nospam@fake.com>
2023-04-05 21:09:02 -0400

    Add NAT gw and private subnet for worker nodes

--- main.tf
+++ main.tf
@@ -20,10 +20,18 @@ resource "aws_vpc" "openshift_vpc" {
   }
 }
 
-resource "aws_subnet" "openshift_subnet" {
+resource "aws_subnet" "controlplane_subnet" {
   vpc_id                  = aws_vpc.openshift_vpc.id
   cidr_block              = var.subnet_cidr
   map_public_ip_on_launch = "true"
+  tags = {
+    Name = var.stack_name
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.openshift_vpc.id
+  cidr_block = var.private_subnet_cidr
   tags = {
     Name = var.stack_name
   }
@@ -103,9 +111,38 @@ resource "aws_route" "cluster_route_to_internet" {
   gateway_id             = aws_internet_gateway.cluster_igw.id
 }
 
+resource "aws_nat_gateway" "nat_gw" {
+  subnet_id         = aws_subnet.private_subnet.id
+  connectivity_type = "private"
+
+  tags = {
+    Name = "OpenShift NAT gw"
+  }
+}
+
+resource "aws_route_table" "private_subnet_route_table" {
+  vpc_id = aws_vpc.openshift_vpc.id
+
+  tags = {
+    Name = "Openshift Cluster Private Route Table"
+  }
+}
+
+resource "aws_route" "private_route" {
+  route_table_id = aws_route_table.private_subnet_route_table.id
+
+  nat_gateway_id         = aws_nat_gateway.nat_gw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
 resource "aws_route_table_association" "route_as" {
-  subnet_id      = aws_subnet.openshift_subnet.id
+  subnet_id      = aws_subnet.controlplane_subnet.id
   route_table_id = aws_route_table.cluster_route_table.id
+}
+
+resource "aws_route_table_association" "private_route_as" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_subnet_route_table.id
 }
 
 data "aws_ami" "rhel8" {
@@ -130,10 +167,23 @@ resource "aws_instance" "control_plane" {
   instance_type   = var.control_plane_type
   count           = var.control_plane_nodes
   key_name        = var.key_name
-  security_groups = [aws_security_group.control_plane_sg.id]
-  subnet_id       = aws_subnet.openshift_subnet.id
+  security_groups = [aws_security_group.control_plane_sg.id, aws_security_group.cluster_internal_sg.id]
+  subnet_id       = aws_subnet.controlplane_subnet.id
 
   tags = {
     Name = "control_plane-${count.index}"
+  }
+}
+
+resource "aws_instance" "worker_node" {
+  ami             = data.aws_ami.rhel8.id
+  instance_type   = var.worker_node_type
+  count           = var.worker_nodes
+  key_name        = var.key_name
+  security_groups = [aws_security_group.cluster_internal_sg.id]
+  subnet_id       = aws_subnet.private_subnet.id
+
+  tags = {
+    Name = "worker_node-${count.index}"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,39 +1,53 @@
 variable "stack_name" {
-  type = string
+  type    = string
   default = "OpenShift VPC"
 }
 
 variable "vpc_cidr" {
-  type = string
+  type    = string
   default = "10.0.0.0/16"
 }
 
 variable "subnet_cidr" {
-  type = string
-  default = "10.0.1.0/28"
+  type    = string
+  default = "10.0.0.0/24"
+}
+
+variable "private_subnet_cidr" {
+  type    = string
+  default = "10.0.1.0/24"
 }
 
 variable "aws_region" {
-  type = string
+  type    = string
   default = "us-east-1"
 }
 
 variable "login_ip" {
-  type = string
+  type    = string
   default = "1.2.3.4/32"
 }
 
 variable "control_plane_type" {
-  type = string
+  type    = string
   default = "t2.micro"
 }
 
+variable "worker_node_type" {
+  type    = string
+  default = "t2.micro"
+}
 variable "control_plane_nodes" {
-  type = number
-  default = 2
+  type    = number
+  default = 1
+}
+
+variable "worker_nodes" {
+  type    = number
+  default = 1
 }
 
 variable "key_name" {
-  type = string
+  type    = string
   default = "key"
 }
